346febb2b7ef6231890344ec2f2c7c455ea6a060
alexandriax <166084006+alexandriax@users.noreply.github.com>
2025-02-09 12:05:14 -0500

    mobile layout implementation

--- js/cherry.js
+++ js/cherry.js
@@ -3,9 +3,9 @@ const cherry = document.getElementById('cherry');
 cherry.addEventListener('mouseenter', () => {
     cherry.classList.add('bounce');
 
-    // Remove and re-add the class to restart the animation on each hover
+    
     setTimeout(() => {
         cherry.classList.remove('bounce');
-    }, 600); // Matches the animation duration
+    }, 600); 
 });
 
